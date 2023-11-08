de55d758bed51430ce0f2209c32680dbb414c072
Luke Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2023-11-07 19:54:44 -0700

    Update run.sh

--- Perceptron/run.sh
+++ Perceptron/run.sh
@@ -3,6 +3,9 @@
 # Activate the virtual environment (if applicable)
 source venv/bin/activate
 
+# Install Libraries
+pip install pandas
+pip install numpy
 # Execute the Python script
 python perceptron.py
 
